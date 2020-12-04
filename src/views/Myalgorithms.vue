d02b32dc020d70c714dca500b81c77998a0baaa0
Brandon Southwick <southwick.brandon21@gmail.com>
2020-12-04 14:02:15 -0700

    Add style

--- src/views/Myalgorithms.vue
+++ src/views/Myalgorithms.vue
@@ -125,3 +125,73 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+/* Suggestions */
+.suggestions {
+  width: 200px;
+  border: 1px solid #ccc;
+}
+
+.suggestion {
+  min-height: 20px;
+}
+
+.suggestion:hover {
+  background-color: #5BDEFF;
+  color: #fff;
+}
+
+.image h2 {
+  font-style: italic;
+  font-size: 1em;
+}
+
+.heading {
+  display: flex;
+  margin-bottom: 20px;
+  margin-top: 20px;
+}
+
+.heading h2 {
+  margin-top: 8px;
+  margin-left: 10px;
+}
+
+.add,
+.edit {
+  display: flex;
+}
+
+.circle {
+  border-radius: 50%;
+  width: 18px;
+  height: 18px;
+  padding: 8px;
+  background: #333;
+  color: #fff;
+  text-align: center
+}
+
+/* Form */
+input,
+textarea,
+select,
+button {
+  font-family: 'Montserrat', sans-serif;
+  font-size: 1em;
+}
+
+.form {
+  margin-right: 50px;
+}
+
+/* Uploaded images */
+.upload h2 {
+  margin: 0px;
+}
+
+.upload img {
+  max-width: 300px;
+}
+</style>
